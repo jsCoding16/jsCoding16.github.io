6c26fff8141d823e92318422ee8d2db67a5587b5
Joao P. <jscoding.contact@gmail.com>
2026-07-23 15:06:09 +0100

    Add files via upload

--- js/diru5kg9l.js
+++ js/diru5kg9l.js
@@ -5,6 +5,7 @@
             var f2 =document.getElementById('fx2');
             var mn0 =document.getElementById('menuBtn');
             var mn2 =document.getElementById('menuBtn2');
+            var mnor = document.getElementById('orcamento');
             a1.addEventListener( 'mousedown', function () {
                 mn2.style.visibility='visible';
                 a1.style.visibility='hidden';
@@ -12,10 +13,12 @@
             a2.addEventListener( 'mousedown', function () {
                 mn0.style.visibility='visible';
                 a2.style.visibility='hidden';
+                mnor.style.visibility='hidden';
             }, false );
             f0.addEventListener( 'mousedown', function () {
                 mn0.style.visibility='hidden';
                 a2.style.visibility='visible';
+                mnor.style.visibility='visible';
             }, false );
             f2.addEventListener( 'mousedown', function () {
                 mn2.style.visibility='hidden';
